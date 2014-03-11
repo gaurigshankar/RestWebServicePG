b5c1a72a94af95840cc3a263e2ae8de240626a31
Gauri Shankar P <gshankar@walmart.com>
2014-03-10 18:14:24 -0700

    Added PathParam to pass input in the path

--- src/main/java/com/gauri/rest/poc/ModifyMigratedFlag.java
+++ src/main/java/com/gauri/rest/poc/ModifyMigratedFlag.java
@@ -3,6 +3,7 @@ package com.gauri.rest.poc;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -11,16 +12,22 @@ import com.gauri.rest.poc.data.Payload;
 
 //import com.google.gson.Gson;
 
-@Path("/flag")
+@Path("/accountServices")
 public class ModifyMigratedFlag {
+	
+	@Path("/customer/{cId}")
 	@PUT
 	@Consumes(MediaType.APPLICATION_JSON)
     @Produces("application/json")
-    public String resetMigratedFlag(InputData inputData){
+    public String resetMigratedFlag(@PathParam("cId") String customerId,InputData inputData){
 	 String response = null;
         try{
+        	System.out.println("customerID:: " +customerId);
         	if(inputData!=null){
         	System.out.println(inputData.getPayload().getPerson().getAccounts()[0].getIsMigrated());
+        	if(!inputData.getPayload().getPerson().getAccounts()[0].getIsMigrated()){
+        		return "{ \"flag\":\"false\"}";
+        	}
         	}
         	
         	
@@ -31,6 +38,6 @@ public class ModifyMigratedFlag {
       }
        // Gson gson = new Gson();
   
-        return "{ \"json\":\"value\"}";
+        return "{ \"flag\":\"true\"}";
     }
 }
